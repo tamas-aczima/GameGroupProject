f0728f7b7294fb608c537842a6429de4ad6b2d8d
Tamas Aczima <tamas.aczima@bossastudios.com>
2017-10-09 16:59:22 +0100

    initial commit

---
+++ myGame/source/Game/RenderingGame.h
@@ -0,0 +1,57 @@
+#pragma once
+
+#include "Common.h"
+#include "Game.h"
+
+using namespace Library;
+
+namespace DirectX
+{
+	class SpriteBatch;
+	class SpriteFont;
+}
+
+namespace Library
+{
+	class FpsComponent;
+	class Keyboard;
+	class Mouse;
+	class FirstPersonCamera;
+	class FpsComponent;
+	class RenderStateHelper;
+}
+
+namespace Rendering
+{
+	class TriangleDemo;
+
+	class RenderingGame : public Game
+	{
+	public:
+		RenderingGame(HINSTANCE instance, const std::wstring& windowClass, const std::wstring& windowTile, int showCommand);
+		~RenderingGame();
+
+		virtual void Initialize() override;
+		virtual void Update(const GameTime& gameTime) override;
+		virtual void Draw(const GameTime& gameTime) override;
+
+	protected:
+		virtual void Shutdown() override;
+
+	private:
+		static const XMVECTORF32 BackGroundColor;
+
+		LPDIRECTINPUT8 mDirectInput;
+		Keyboard* mKeyboard;
+		Mouse* mMouse;
+		FirstPersonCamera* mCamera;
+		FpsComponent* mFpsComponent;
+
+		TriangleDemo* mDemo;
+
+		SpriteBatch* mSpriteBatch;
+		SpriteFont* mSpriteFont;
+
+		XMFLOAT2 mMouseTextPosition;
+	};
+}
